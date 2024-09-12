7730cd58cc9b97cc4530248c12b9231a271763cb
Mariam Hosni <mariam22hosni@gmail.com>
2024-09-12 21:57:19 +0100

    added a python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1= 10
+number2= 5
+sum= number1+number2
+print(f"Addition of {number1} and {number2} is {sum}")
+sub= number1-number2
+print(f"Subtraction of {number1} and {number2} is {sub}")
+mul= number1*number2
+print(f"Multiplication of {number1} and {number2} is {mul}")
+
